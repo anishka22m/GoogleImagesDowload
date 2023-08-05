389322a4a9375a1810c7d5bfdd1f713a12f35093
anishka22m <97446005+anishka22m@users.noreply.github.com>
2023-08-05 12:04:51 +0530

    Add files via upload

---
+++ MiniProject10_GoogleImagesDownload.py
@@ -0,0 +1,6 @@
+#Find the package on pypi.org 
+#install the package using pip command on CMD
+#import the package function and use to download bulk google images
+
+from pygoogle_image import image as pi
+pi.download(keywords='Spider Man', limit=10)
